2ff564678dfad599c2ca48382570f2584d1a0f4a
Leonardo Martinez <leomartinezjr@hotmail.com>
2026-06-18 17:28:15 -0300

    Initial Commit

---
+++ ExpenseAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseAssistant
+//
+//  Created by Luana Martinez on 6/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenseAssistant/ExpenseAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseAssistantApp.swift
+//  ExpenseAssistant
+//
+//  Created by Luana Martinez on 6/18/26.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
